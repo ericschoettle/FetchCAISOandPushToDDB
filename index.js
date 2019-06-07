e203bc9ee79601f6e07e209ccfa3c49587134e43
ericschoettle <eric.schoettle@gmail.com>
2019-06-07 15:35:21 -0700

    Add arbitrage algorithm to data processing

--- index.js
+++ index.js
@@ -115,21 +115,18 @@
 
 
 const AWS = require('aws-sdk');
-// AWS.config.update({region: 'REGION'});
 const dynamoDB = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 const http = require('http');
 const csv = require('csv-parser');
 const unzipper = require('unzipper');
-const xml = require('xml2js');
-const parser = new xml.Parser();
 const concat = require('concat-stream');
-// const unzipper = require('adm-zip');
+const algorithm = require('./algorithm.js');
 const moment = require('moment');
 
 
 const fetchAndTransform = async (event) => {
   const startDate = event.iterator.startDate || 20160101;
-  const endDate = event.iterator.endDate || 20160201;
+  const endDate = event.iterator.endDate || 201601030;
   const nodeId = event.nodeId || 'LAPLMG1_7_B2';
   // Questions - for LAPLMG1_7_B2, the earlist I an start is around 15/11 - is this true for all nodes?
   // API only accepts one month at a time :/
@@ -151,57 +148,77 @@ const fetchAndTransform = async (event) => {
       let entryCount = 0;
       let recordCount = 0;
       let putBatchCount = 0;
+
+      let hours = []; 
       resp.pipe(unzipper.Parse()) // Unzip
         .on('entry', (entry)=>{
           // resolve({status: 200, position: 'unzip'})
           const fileName = entry.path;
           if (fileName.match(/\.csv/)) {
             entryCount ++;
-            let putBatch = [];
+
             // Needs logic to go to CSV if CSV, and return xml as error if it gives XML (b/c that's what the API does when you ask for a CSV and there's an error)   
             entry.pipe(csv())
               .on('data', (data) => {
                 if (data.LMP_TYPE === 'LMP') {
                   recordCount++
-                  const createDate = moment().format();
-                  putBatch.push({
-                    PutRequest: {
-                      Item: {
-                        // 'ID': {S: `${data.NODE}-${data.OPR_DT}-${data.OPR_HR}`},
-                        'nodeId': {S: data.NODE},
-                        'date': {S: data.OPR_DT},
-                        'operatingHour': {N: data.OPR_HR},
-                        'startTimeGMT': {S: data.INTERVALSTARTTIME_GMT},
-                        'endTimeGMT': {S: data.INTERVALSTARTTIME_GMT},
-                        'price': {N: data.MW},
-                        'duration': {N: '60'},
-                        'createDate': {S: createDate}
-                      }
-                    }
+                  hours.push({
+                    // 'ID': {S: `${data.NODE}-${data.OPR_DT}-${data.OPR_HR}`},
+                    'nodeId': data.NODE,
+                    'date': data.OPR_DT,
+                    'operatingHour': data.OPR_HR,
+                    'startTimeGMT': data.INTERVALSTARTTIME_GMT,
+                    'endTimeGMT': data.INTERVALENDTIME_GMT,
+                    'price': data.MW,
+                    'duration': '60',
                   });
                 } 
-                if (putBatch.length === 24) {
-                  putBatchCount++
-                  const params = {
-                    RequestItems: {
-                      'CA-Hours-DAM-LMP': putBatch
-                    }
-                  }
-                  dynamoDB.batchWriteItem(params, (err, data)=>{
-                    if (err) {
-                      //TODO - RETURN an error type. Implement a repeat policy in step function. 
-                      console.log(`Error on write to ddb: ${err}`);
-                      errors.push(`Error on write to ddb: ${err}`);
-                    } 
-                  });
-                  putBatch = [];
-                } else if (putBatch.length > 24) {
-                  console.log(`Error on put batch - length: ${putBatch.length}`);
-                  errors.push(`Error on put batch - length: ${putBatch.length}`);
-                } 
-  
               })
               .on('end', ()=>{
+                // sort hours
+                hours.sort((prev,curr)=>{
+                  return new Date(prev.startTimeGMT) - new Date(curr.startTimeGMT)
+                })
+                // add prices
+                let test = algorithm(hours, 0, 3);
+
+                // PUT to dynamoDB
+                for (let i = 0; i < days.length; i++) {
+                  if (i % 24 === 24 - 1) { // full day
+                    let day = days.slice(i - 23, i + 1)
+                    debugger;
+                    const createDate = moment().format();
+                    let putBatch = day.map(hour=>{
+                      return {PutRequest: {
+                        Item: {
+                          'nodeId': {S: hour.nodeId},
+                          'date': {S: hour.date},
+                          'operatingHour': {N: hour.operatingHour},
+                          'startTimeGMT': {S: hour.startTimeGMT},
+                          'endTimeGMT': {S: hour.endTimeGMT},
+                          'price': {N: hour.price},
+                          'duration': {N: '60'},
+                          'createDate': {S: createDate}
+                        }
+                      }}
+                    });
+                    putBatchCount++
+                    const params = {
+                      RequestItems: {
+                        'CA-Hours-DAM-LMP': putBatch
+                      }
+                    }
+                    dynamoDB.batchWriteItem(params, (err, data)=>{
+                      if (err) {
+                        //TODO - RETURN an error type. Implement a repeat policy in step function. 
+                        console.log(`Error on write to ddb: ${err}`);
+                        errors.push(`Error on write to ddb: ${err}`);
+                      } 
+                    });
+                  }       
+                }
+
+                // serve response
                 if (errors.length) {
                   const response = {
                     statusCode: 512,
@@ -266,6 +283,6 @@ const fetchAndTransform = async (event) => {
   });
 };
 
-// console.log(Promise.resolve(fetchAndTransform({iterator: {}})));
+console.log(Promise.resolve(fetchAndTransform({iterator: {}})));
 
 exports.fetchAndTransform = fetchAndTransform;
